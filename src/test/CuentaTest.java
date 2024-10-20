12df799ee32be421a6a99baa355255f353160e57
Rafael <rafaelgonzalezrodriguez0@gmail.com>
2024-10-20 17:45:42 +0200

    Commit funciona bien el devolver saldo

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -12,11 +12,12 @@ import src.Cuenta;
 
 class CuentaTest {
 	
-	Cuenta cuenta1 = new Cuenta("12345", "pepe", 50);
-	Cuenta cuenta2 = new Cuenta("67890", "manu", 0);
+	Cuenta cuenta1 = new Cuenta("12345", "pepe", 50f);
+	Cuenta cuenta2 = new Cuenta("67890", "manu", 0f);
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	@AfterAll
@@ -25,6 +26,8 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		
 	}
 
 	@AfterEach
@@ -33,38 +36,28 @@ class CuentaTest {
 
 	@Test
 	void test0014a() {
-		cuenta1.retirar(200);
-		cuenta2.retirar(350);
-		cuenta1.ingresar(100);
-		cuenta2.retirar(200);
-		cuenta2.retirar(150);
-		cuenta1.retirar(200);
-		cuenta2.ingresar(50);
-		cuenta1.retirar(100);
+		cuenta1.retirar(200f);
+		cuenta1.ingresar(100f);
+		cuenta1.retirar(200f);
 		
 		System.out.println("Cuenta 1: " + cuenta1.getSaldo() + " €");
-		System.out.println("Cuenta 2: " + cuenta2.getSaldo() + " €");
 		
 		/*Ccomprobación cuenta 1*/
-		assertEquals(-250, cuenta1.getSaldo());
+		assertEquals(-250f, cuenta1.getSaldo());
 	}
 	
 	@Test
 	void test0014b() {
-		cuenta1.retirar(200);
-		cuenta2.retirar(350);
-		cuenta1.ingresar(100);
-		cuenta2.retirar(200);
-		cuenta2.retirar(150);
-		cuenta1.retirar(200);
-		cuenta2.ingresar(50);
-		cuenta1.retirar(100);
+		cuenta2.retirar(350f);
+		cuenta2.retirar(200f);
+		cuenta2.retirar(150f);
+		cuenta2.ingresar(50f);
+		cuenta2.retirar(100f);
 		
-		System.out.println("Cuenta 1: " + cuenta1.getSaldo() + " €");
 		System.out.println("Cuenta 2: " + cuenta2.getSaldo() + " €");
 		
 		/*Ccomprobación cuenta 2*/
-		assertEquals(-450, cuenta2.getSaldo());
+		assertEquals(-750f, cuenta2.getSaldo());
 	}
 
 }
